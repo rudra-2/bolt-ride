b813349334bfb474e4262e0251bef9abfb3e9fd7
rudra-2 <rmp20026@gmail.com>
2025-08-15 13:14:08 +0530

    client and server connected

--- admin-app/client/src/App.js
+++ admin-app/client/src/App.js
@@ -1,12 +1,24 @@
+
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [info, setInfo] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8000/info/')
+      .then(res => res.json())
+      .then(data => setInfo(data.message))
+      .catch(() => setInfo('Could not fetch backend info'));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to Bolt Ride Partner</h1>
+        <p style={{ marginTop: 20 }}>{info}</p>
       </header>
     </div>
   );
--- admin-app/server/server/settings.py
+++ admin-app/server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'web'
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
